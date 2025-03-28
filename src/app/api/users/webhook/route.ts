156f5c38a1730fc7b80b8e3f27bac60f45000dbd
Ɓʘɓƴ <shboby71@gmail.com>
2025-03-28 22:49:05 +0530

    !Users webhook setup with database

--- src/app/api/users/webhook/route.ts
+++ src/app/api/users/webhook/route.ts
@@ -1,13 +1,16 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
+import { db } from '@/db'
+import { users } from '@/db/schema'
+import { eq } from 'drizzle-orm'
 
 export async function POST(req: Request) {
     const SIGNING_SECRET = process.env.SIGNING_SECRET
-
     if (!SIGNING_SECRET) {
         throw new Error('Error: Please add SIGNING_SECRET from Clerk Dashboard to .env or .env')
     }
+
 
     // Create new Svix instance with secret
     const wh = new Webhook(SIGNING_SECRET)
@@ -28,7 +31,6 @@ export async function POST(req: Request) {
     // Get body
     const payload = await req.json()
     const body = JSON.stringify(payload)
-
     let evt: WebhookEvent
 
     // Verify payload with headers
@@ -47,10 +49,34 @@ export async function POST(req: Request) {
 
     // Do something with payload
     // For this guide, log payload to console
-    const { id } = evt.data
     const eventType = evt.type
-    console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
-    console.log('Webhook payload:', body)
+    if (eventType === "user.created") {
+        const { data } = evt
+        await db.insert(users).values({
+            clerkId: data.id,
+            name: `${data.first_name} ${data.last_name}`,
+            imageUrl: data.image_url,
+        });
+    }
 
+
+    if (eventType === "user.deleted") {
+        const { data } = evt
+
+        if (!data.id) {
+            return new Response("Users Id is Missing :", { status: 400 })
+        }
+        await db.delete(users).where(eq(users.clerkId, data.id))
+    }
+
+    if (eventType === "user.updated") {
+        const { data } = evt
+        await db.update(users).set({
+            name: `${data.first_name} ${data.last_name}`,
+            imageUrl: data.image_url,
+        })
+            .where(eq(users.clerkId, data.id))
+
+    }
     return new Response('Webhook received', { status: 200 })
 }
