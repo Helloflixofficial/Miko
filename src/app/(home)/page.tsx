e3b8ab08302ebbf72162b9ae80768d5627aa8ae2
Ɓʘɓƴ <shboby71@gmail.com>
2025-04-04 22:14:13 +0530

    trcp str..

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,12 @@
-export default function Home() {
+// "use client";
+import { trpc } from "@/trpc/server";
+export default async function Home() {
+  const data = await trpc.hello({ text: "Boby" });
   return (
     <>
-      <p className="text-xl font-semibold tracking-tight">Firetube Content Here</p>
+      <p className="text-xl font-semibold tracking-tight">
+        Firetube Content: {data.greeting}
+      </p>
     </>
   );
 }
