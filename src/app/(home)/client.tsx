bb3a18acfebf7c2107a85302ac0fba728a9e6d96
Ɓʘɓƴ <shboby71@gmail.com>
2025-05-21 22:58:12 +0530

    trcp file setup..

--- src/app/(home)/client.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import { trpc } from "@/trpc/client";
-import React from "react";
-export const Client = () => {
-  const [data] = trpc.categories.getMany.useSuspenseQuery();
-  return <div>{JSON.stringify(data)}</div>;
-};
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,26 +1,18 @@
+import { HomeView } from "@/modules/home/ui/views/home-view";
 import { HydrateClient, trpc } from "@/trpc/server";
-import { Client } from "./client";
-import { Suspense } from "react";
-import { ErrorBoundary } from "react-error-boundary";
-export default async function Home() {
+export const dynamic = "force-dynamic";
+
+interface pageProps {
+  searchParams: Promise<{ categoryId?: string }>;
+}
+
+const page = async ({ searchParams }: pageProps) => {
+  const { categoryId } = await searchParams;
   void trpc.categories.getMany.prefetch();
   return (
     <HydrateClient>
-      <Suspense
-        fallback={
-          <p className="text-xl font-semibold tracking-tight">Loading...</p>
-        }
-      >
-        <ErrorBoundary
-          fallback={
-            <p className="text-xl font-semibold tracking-tight">Erorr..</p>
-          }
-        >
-          <p className="text-xl font-semibold tracking-tight">
-            Firetube Content: <Client />
-          </p>
-        </ErrorBoundary>
-      </Suspense>
+      <HomeView categoryId={categoryId} />
     </HydrateClient>
   );
-}
+};
+export default page;
