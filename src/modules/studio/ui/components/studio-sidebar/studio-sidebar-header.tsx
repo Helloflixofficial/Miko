54a409f50363cc640640d90c3c6c8deee06fc268
Ɓʘɓƴ <shboby71@gmail.com>
2025-06-11 21:57:49 +0530

    profile structures & responsive

--- src/modules/studio/ui/components/studio-sidebar/studio-sidebar-header.tsx
+++ src/modules/studio/ui/components/studio-sidebar/studio-sidebar-header.tsx
@@ -1,19 +1,58 @@
-import { SidebarHeader } from "@/components/ui/sidebar";
+import {
+  SidebarHeader,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "@/components/ui/sidebar";
 import { useUser } from "@clerk/nextjs";
 import { UserAvatar } from "@/components/user-avatar";
 import Link from "next/link";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export const StudioSidebarHeader = () => {
   const { user } = useUser();
-  if (!user) return null;
+  const { state } = useSidebar();
+  if (!user) {
+    return (
+      <SidebarHeader className="flex flex-col items-center pb-4">
+        <Skeleton className="size-[112px] rounded-full" />
+        <div className="flex flex-col items-center gap-y-2">
+          <Skeleton className="h-4 w-[80px]" />
+          <Skeleton className="h-4 w-[100px]" />
+        </div>
+      </SidebarHeader>
+    );
+  }
+
+  if (state === "collapsed") {
+    return (
+      <SidebarMenuItem>
+        <SidebarMenuButton tooltip="Your Profile" asChild>
+          <Link href="users/current">
+            <UserAvatar
+              imageUrl={user.imageUrl}
+              name={user.fullName ?? "User"}
+              size="xs"
+            />
+            <span className="text-sm">Your Profile</span>
+          </Link>
+        </SidebarMenuButton>
+      </SidebarMenuItem>
+    );
+  }
   return (
     <SidebarHeader className="flex items-center justify-center pb-4">
-      <Link href="/user/current"></Link>
-      <UserAvatar
-        imageUrl={user.imageUrl}
-        name={user.fullName ?? "User"}
-        className="size-[112px] hover:opacity-80 transition-opacity "
-      />
+      <Link href="/users/current">
+        <UserAvatar
+          imageUrl={user.imageUrl}
+          name={user.fullName ?? "User"}
+          className="size-[112px] hover:opacity-80 transition-opacity"
+        />
+      </Link>
+      <div className="flex flex-col items-center gap-y-2">
+        <p className="text-sm font-medium">User Profile</p>
+        <p className="text-xs text-muted-foreground">{user.fullName}</p>
+      </div>
     </SidebarHeader>
   );
 };
